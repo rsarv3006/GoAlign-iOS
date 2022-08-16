76f10669dba8affd2c436c77dc90ad36b2b349e9
Robby S Engineer <robby.sarvis@akidolabs.com>
2022-08-15 19:00:13 -0500

    Add optional enum for auto correct on Text Form Component

--- YourTurn/Model/Form Components/TextFormComponent.swift
+++ YourTurn/Model/Form Components/TextFormComponent.swift
@@ -12,16 +12,19 @@ final class TextFormComponent: FormComponent {
     let placeholder: String
     let keyboardType: UIKeyboardType
     let isSecureTextEntryEnabled: Bool
+    let autoCorrectionType: UITextAutocorrectionType
     
     init(id: FormField,
          placeholder: String,
          keyboardType: UIKeyboardType = .default,
-         validations: [ValidationManager] = [],
-         isSecureTextEntryEnabled: Bool = false
+         isSecureTextEntryEnabled: Bool = false,
+         autoCorrectionType: UITextAutocorrectionType = .default,
+         validations: [ValidationManager] = []
     ) {
         self.placeholder = placeholder
         self.keyboardType = keyboardType
         self.isSecureTextEntryEnabled = isSecureTextEntryEnabled
+        self.autoCorrectionType = autoCorrectionType
         super.init(id, validations: validations)
     }
 }
