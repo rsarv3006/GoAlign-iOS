2a27e414710f16df272e599492aeef5ce072d1e7
Robby Sarvis <rjs.app.dev@gmail.com>
2023-10-15 22:56:53 -0500

    linting EditAssignedTeamMemberModal

--- YourTurn/TaskAddEditScreen/EditAssignedTeamMemberModal.swift
+++ YourTurn/TaskAddEditScreen/EditAssignedTeamMemberModal.swift
@@ -9,9 +9,8 @@ import UIKit
 import Combine
 
 class EditAssignedTeamMemberModal: ModalViewController {
-    
     private var subscriptions = Set<AnyCancellable>()
-    
+
     var viewModel: EditAssignedTeamMemberModalVM? {
         didSet {
             guard let viewModel = viewModel else { return }
@@ -22,77 +21,84 @@ class EditAssignedTeamMemberModal: ModalViewController {
                         let indexPath = IndexPath(row: indexOfAssignedUser, section: 0)
                         self.teamMembersTableView.selectRow(at: indexPath, animated: true, scrollPosition: .bottom)
                         self.tableView(self.teamMembersTableView, didSelectRowAt: indexPath)
-                        
                     }
                 }
             }.store(in: &subscriptions)
-            
+
             viewModel.requestShowError.sink { error in
                 self.showMessage(withTitle: "Uh Oh", message: error.localizedDescription)
             }.store(in: &subscriptions)
         }
     }
-    
+
     // MARK: - UI Elements    
     private lazy var titleLabel: UILabel = {
         let title = UILabel()
         title.text = "Assigned Team Member"
         return title
     }()
-    
+
     private lazy var teamMembersTableView: UITableView = {
-        let tv = UITableView()
-        tv.backgroundColor = .systemGray3
-        return tv
+        let tableView = UITableView()
+        tableView.backgroundColor = .systemGray3
+        return tableView
     }()
-    
+
     private lazy var closeButton: StandardButton = {
         let button = StandardButton()
         button.setTitle("CLOSE", for: .normal)
         return button
     }()
-    
+
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureModal()
         configureTableView()
-        
+
         viewModel?.fetchTeamMembers()
     }
-    
+
     // MARK: - Helpers
     func configureModal() {
         view.addSubview(subView)
         subView.center(inView: view)
-        
+
         let screenHeight = UIScreen.main.bounds.size.height
         let screenWidth = UIScreen.main.bounds.size.width
-        
+
         subView.setWidth(screenWidth * 0.75)
         subView.setHeight(screenHeight * 0.6)
-        
+
         subView.addSubview(titleLabel)
         titleLabel.centerX(inView: subView, topAnchor: subView.topAnchor, paddingTop: 8)
-        
+
         subView.addSubview(closeButton)
         closeButton.centerX(inView: subView)
-        closeButton.anchor(left: subView.leftAnchor, bottom: subView.bottomAnchor, right: subView.rightAnchor, paddingLeft: 12, paddingBottom: 12, paddingRight: 12)
+        closeButton.anchor(
+            left: subView.leftAnchor,
+            bottom: subView.bottomAnchor,
+            right: subView.rightAnchor,
+            paddingLeft: 12,
+            paddingBottom: 12,
+            paddingRight: 12)
         closeButton.addTarget(self, action: #selector(onClosePressed), for: .touchUpInside)
-        
+
         subView.addSubview(teamMembersTableView)
-        teamMembersTableView.anchor(top: titleLabel.bottomAnchor, left: subView.leftAnchor, bottom: closeButton.topAnchor, right: subView.rightAnchor)
-        
-        
+        teamMembersTableView.anchor(
+            top: titleLabel.bottomAnchor,
+            left: subView.leftAnchor,
+            bottom: closeButton.topAnchor,
+            right: subView.rightAnchor)
     }
-    
+
     func configureTableView() {
         teamMembersTableView.delegate = self
         teamMembersTableView.dataSource = self
         teamMembersTableView.rowHeight = 40
         teamMembersTableView.register(TeamSelectModalCellView.self, forCellReuseIdentifier: "otherThing")
     }
-    
+
     @objc func onClosePressed() {
         self.dismiss(animated: true)
     }
@@ -111,13 +117,16 @@ extension EditAssignedTeamMemberModal: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return viewModel?.teamMembers.count ?? 0
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "otherThing", for: indexPath) as! TeamSelectModalCellView
-        if let teamMembersArray = viewModel?.teamMembers {
-            cell.nameLabelString = teamMembersArray[indexPath.row].username
+        let cell = tableView.dequeueReusableCell(withIdentifier: "otherThing", for: indexPath)
+
+        if let teamSelectCell = cell as? TeamSelectModalCellView,
+           let teamMembersArray = viewModel?.teamMembers {
+            teamSelectCell.nameLabelString = teamMembersArray[indexPath.row].username
+            return teamSelectCell
         }
+
         return cell
     }
 }
-
