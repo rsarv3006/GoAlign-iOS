501e500dbc9b9bce1a8c840c71d74314dbe3cff9
Robby Sarvis <rjs.app.dev@gmail.com>
2023-10-16 16:30:19 -0500

    linting ui tests

--- YourTurnUITests/YourTurnUITests.swift
+++ YourTurnUITests/YourTurnUITests.swift
@@ -16,7 +16,6 @@ class YourTurnUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
         app = XCUIApplication()
         guard let app = app else {
             return
@@ -35,12 +34,18 @@ class YourTurnUITests: XCTestCase {
         app.buttons["Already have an Account?  Click here to Sign In!"].tap()
 
         let collectionViewsQuery = app.collectionViews
+        // swiftlint:disable:next line_length
         collectionViewsQuery/*@START_MENU_TOKEN@*/.cells.textFields["Email Address"]/*[[".cells.textFields[\"Email Address\"]",".textFields[\"Email Address\"]"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/.tap()
         collectionViewsQuery.cells.textFields["Email Address"].typeText(credentials!.username)
 
+        // swiftlint:disable:next line_length
         collectionViewsQuery/*@START_MENU_TOKEN@*/.secureTextFields["Password"]/*[[".cells.secureTextFields[\"Password\"]",".secureTextFields[\"Password\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        collectionViewsQuery/*@START_MENU_TOKEN@*/.secureTextFields["Password"]/*[[".cells.secureTextFields[\"Password\"]",".secureTextFields[\"Password\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.typeText(credentials!.password)
+        collectionViewsQuery
+        // swiftlint:disable:next line_length
+        /*@START_MENU_TOKEN@*/.secureTextFields["Password"]/*[[".cells.secureTextFields[\"Password\"]",".secureTextFields[\"Password\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+            .typeText(credentials!.password)
 
+        // swiftlint:disable:next line_length
         collectionViewsQuery/*@START_MENU_TOKEN@*/.buttons["Sign In!"]/*[[".cells.buttons[\"Sign In!\"]",".buttons[\"Sign In!\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         app.staticTexts["My Tasks"].tap()
 
@@ -54,6 +59,7 @@ class YourTurnUITests: XCTestCase {
         teamNameField.tap()
         teamNameField.typeText("TestTeam")
 
+        // swiftlint:disable:next line_length
         app/*@START_MENU_TOKEN@*/.buttons["Return"]/*[[".keyboards",".buttons[\"return\"]",".buttons[\"Return\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
         app.buttons["Create Team"].tap()
         app.staticTexts["My Tasks"].tap()
@@ -63,6 +69,7 @@ class YourTurnUITests: XCTestCase {
     func testGroupStatsTabs() throws {
         guard let app = app else { return }
 
+        // swiftlint:disable:next line_length
         app.tables/*@START_MENU_TOKEN@*/.cells.containing(.staticText, identifier: "GroupWithNoName").element/*[[".cells.containing(.staticText, identifier:\"Tasks: 6\").element",".cells.containing(.staticText, identifier:\"GroupWithNoName\").element"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         app.tabBars["Tab Bar"].buttons["Stats"].tap()
         app.staticTexts["Team Stats"].tap()
@@ -71,6 +78,7 @@ class YourTurnUITests: XCTestCase {
     func testDrawerTeamInvites() throws {
         guard let app = app else { return }
         app.buttons["drag"].tap()
+        // swiftlint:disable:next line_length
         app/*@START_MENU_TOKEN@*/.staticTexts["Invites"]/*[[".buttons[\"Invites\"].staticTexts[\"Invites\"]",".staticTexts[\"Invites\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         app.navigationBars["Team Invites"].staticTexts["Team Invites"].tap()
     }
@@ -78,7 +86,10 @@ class YourTurnUITests: XCTestCase {
     func testDrawerSettingsDelete() throws {
         guard let app = app else { return }
         app.buttons["drag"].tap()
-        app/*@START_MENU_TOKEN@*/.staticTexts["Settings"]/*[[".buttons[\"Settings\"].staticTexts[\"Settings\"]",".staticTexts[\"Settings\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.staticTexts["Settings"]
+            .tap()
+
+        // swiftlint:disable:next line_length
         app.tables/*@START_MENU_TOKEN@*/.buttons["Delete Account"]/*[[".cells.buttons[\"Delete Account\"]",".buttons[\"Delete Account\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
 
     }
