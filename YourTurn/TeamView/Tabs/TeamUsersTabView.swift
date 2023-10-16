667a536ad54efb8f7e26490d4608383f262fc20a
Robby Sarvis <rjs.app.dev@gmail.com>
2023-10-16 15:44:31 -0500

    team users tab view listing

--- YourTurn/TeamView/Tabs/TeamUsersTabView.swift
+++ YourTurn/TeamView/Tabs/TeamUsersTabView.swift
@@ -19,10 +19,9 @@ private struct TabCellIdentifiers {
 }
 
 class TeamUsersTabView: YtViewController {
-    
     var subscriptions = Set<AnyCancellable>()
     private let selectedBackgroundColor: UIColor = .customAccentColor ?? .systemBlue
-    
+
     private var teamInvitesArray: [TeamInviteModel] = [] {
         didSet {
             DispatchQueue.main.async {
@@ -30,14 +29,14 @@ class TeamUsersTabView: YtViewController {
             }
         }
     }
-    
+
     private var selected: SelectedList = .users {
         didSet {
             guard let viewModel = viewModel else { return }
             if selected == .users {
                 currentUsersButton.backgroundColor = selectedBackgroundColor
                 invitesButton.backgroundColor = .customBackgroundColor
-                
+
                 currentUsersButton.setTitleColor(.lightButtonText, for: .normal)
                 invitesButton.setTitleColor(.customTitleText, for: .normal)
                 tableView.reloadData()
@@ -45,30 +44,29 @@ class TeamUsersTabView: YtViewController {
                 viewModel.fetchTeamInvites()
                 invitesButton.backgroundColor = selectedBackgroundColor
                 currentUsersButton.backgroundColor = .customBackgroundColor
-                
+
                 currentUsersButton.setTitleColor(.customTitleText, for: .normal)
                 invitesButton.setTitleColor(.lightButtonText, for: .normal)
                 tableView.reloadData()
             }
         }
     }
-    
+
     var viewModel: TeamUsersTabVM? {
         didSet {
             guard let viewModel = viewModel else { return }
             if selected == .users {
                 tableView.reloadData()
             }
-            
-            viewModel.teamInvitesSubject.sink { teamInvites in
+
+            viewModel.teamInvitesSubject.sink { _ in
                 if self.selected == .invites {
                     DispatchQueue.main.async {
                         self.tableView.reloadData()
                     }
-                    
                 }
             }.store(in: &subscriptions)
-            
+
             viewModel.shouldShowCreateInviteButton.sink { shouldShowCreateInviteButton in
                 DispatchQueue.main.async {
                     self.clearView()
@@ -77,24 +75,26 @@ class TeamUsersTabView: YtViewController {
                     }
                 }
             }.store(in: &subscriptions)
-            
+
             viewModel.requestTableReload.sink { _ in
                 DispatchQueue.main.async {
                     self.tableView.reloadData()
                 }
             }.store(in: &subscriptions)
-            
+
             viewModel.teamInvitesSubject.sink { teamInviteResult in
-                switch(teamInviteResult) {
+                switch teamInviteResult {
                 case .failure(let error):
-                    self.showMessage(withTitle: "Uh Oh", message: "Error encountered fetching outstanding invites. \(error.localizedDescription)")
+                    self.showMessage(
+                        withTitle: "Uh Oh",
+                        message: "Error encountered fetching outstanding invites. \(error.localizedDescription)")
                 case .success(let teamInvites):
                     self.teamInvitesArray = teamInvites
                 }
             }.store(in: &subscriptions)
         }
     }
-    
+
     // MARK: UI Elements
     private lazy var currentUsersButton: UIButton = {
         let button = UIButton()
@@ -106,7 +106,7 @@ class TeamUsersTabView: YtViewController {
         button.titleLabel?.font = .systemFont(ofSize: 20)
         return button
     }()
-    
+
     private lazy var invitesButton: UIButton = {
         let button = UIButton()
         button.setTitle("Invites", for: .normal)
@@ -117,7 +117,7 @@ class TeamUsersTabView: YtViewController {
         button.titleLabel?.font = .systemFont(ofSize: 20)
         return button
     }()
-    
+
     private lazy var tableView: UITableView = {
         let tableView = UITableView()
         tableView.register(TeamInvitesTabViewCell.self, forCellReuseIdentifier: TabCellIdentifiers.invites)
@@ -126,7 +126,7 @@ class TeamUsersTabView: YtViewController {
         tableView.backgroundColor = .customBackgroundColor
         return tableView
     }()
-    
+
     private lazy var inviteUsersButton: BlueButton = {
         let button = BlueButton()
         button.setTitle("Invite Users", for: .normal)
@@ -134,65 +134,94 @@ class TeamUsersTabView: YtViewController {
         button.titleLabel?.font = .systemFont(ofSize: 20)
         return button
     }()
-    
+
     // MARK: LIFECYCLE
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .customBackgroundColor
         navigationController?.isNavigationBarHidden = true
     }
-    
+
     // MARK: HELPERS
     override func configureView() {
         tableView.dataSource = self
         tableView.delegate = self
-        
+
         if viewModel?.shouldShowCreateInviteButton.value == true {
             loadViewWithTeamInviteButton()
         } else {
             loadViewWithoutTeamInviteButton()
         }
     }
-    
+
     private func clearView() {
         inviteUsersButton.removeFromSuperview()
         currentUsersButton.removeFromSuperview()
         invitesButton.removeFromSuperview()
         tableView.removeFromSuperview()
     }
-    
+
     private func loadViewWithTeamInviteButton() {
         let screenWidth = UIScreen.main.bounds.size.width
         let topSafeAnchor = view.safeAreaLayoutGuide.topAnchor
-        
+
         view.addSubview(inviteUsersButton)
         inviteUsersButton.centerX(inView: view, topAnchor: topSafeAnchor)
         inviteUsersButton.anchor(width: screenWidth * 0.6)
-        
+
         view.addSubview(currentUsersButton)
-        currentUsersButton.anchor(top: inviteUsersButton.bottomAnchor, left: view.leftAnchor, paddingTop: 12, paddingLeft: 8, width: screenWidth / 2, height: 44)
-        
+        currentUsersButton.anchor(
+            top: inviteUsersButton.bottomAnchor,
+            left: view.leftAnchor,
+            paddingTop: 12,
+            paddingLeft: 8,
+            width: screenWidth / 2, height: 44)
+
         view.addSubview(invitesButton)
-        invitesButton.anchor(top: inviteUsersButton.bottomAnchor, left: currentUsersButton.rightAnchor, right: view.rightAnchor, paddingTop: 12, paddingRight: 8, height: 44)
-        
+        invitesButton.anchor(
+            top: inviteUsersButton.bottomAnchor,
+            left: currentUsersButton.rightAnchor,
+            right: view.rightAnchor,
+            paddingTop: 12,
+            paddingRight: 8,
+            height: 44)
+
         view.addSubview(tableView)
-        tableView.anchor(top:currentUsersButton.bottomAnchor, left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor)
+        tableView.anchor(
+            top: currentUsersButton.bottomAnchor,
+            left: view.leftAnchor,
+            bottom: view.safeAreaLayoutGuide.bottomAnchor,
+            right: view.rightAnchor)
     }
-    
+
     private func loadViewWithoutTeamInviteButton() {
         let screenWidth = UIScreen.main.bounds.size.width
         let topSafeAnchor = view.safeAreaLayoutGuide.topAnchor
-        
+
         view.addSubview(currentUsersButton)
-        currentUsersButton.anchor(top: topSafeAnchor, left: view.leftAnchor, paddingTop: 12, width: screenWidth / 2, height: 64)
-        
+        currentUsersButton.anchor(
+            top: topSafeAnchor,
+            left: view.leftAnchor,
+            paddingTop: 12,
+            width: screenWidth / 2,
+            height: 64)
+
         view.addSubview(invitesButton)
-        invitesButton.anchor(top: topSafeAnchor, left: currentUsersButton.rightAnchor, right: view.rightAnchor, paddingTop: 12, height: 64)
-        
+        invitesButton.anchor(
+            top: topSafeAnchor,
+            left: currentUsersButton.rightAnchor,
+            right: view.rightAnchor,
+            paddingTop: 12,
+            height: 64)
+
         view.addSubview(tableView)
-        tableView.anchor(top: currentUsersButton.bottomAnchor, left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor)
+        tableView.anchor(
+            top: currentUsersButton.bottomAnchor,
+            left: view.leftAnchor,
+            bottom: view.safeAreaLayoutGuide.bottomAnchor,
+            right: view.rightAnchor)
     }
-    
+
     @objc func onButtonPress() {
         if selected == .users {
             selected = .invites
@@ -200,7 +229,7 @@ class TeamUsersTabView: YtViewController {
             selected = .users
         }
     }
-    
+
     @objc func onInviteTeamMemberPressed() {
         guard let viewModel = viewModel else { return }
         let inviteModal = TeamInviteUserModal()
@@ -223,21 +252,28 @@ extension TeamUsersTabView: UITableViewDataSource {
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if selected == .users, let viewModel = viewModel {
-            let cell = tableView.dequeueReusableCell(withIdentifier: TabCellIdentifiers.users, for: indexPath) as! TeamUsersTabViewCell
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: TabCellIdentifiers.users,
+                // swiftlint:disable:next force_cast
+                for: indexPath) as! TeamUsersTabViewCell
             cell.viewModel = TeamUsersTabViewCellVM(user: viewModel.users[indexPath.row])
             return cell
-            
+
         } else if selected == .invites {
-            let cell = tableView.dequeueReusableCell(withIdentifier: TabCellIdentifiers.invites, for: indexPath) as! TeamInvitesTabViewCell
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: TabCellIdentifiers.invites,
+                // swiftlint:disable:next force_cast
+                for: indexPath) as! TeamInvitesTabViewCell
             cell.viewModel = TeamInvitesTabViewCellVM(invite: teamInvitesArray[indexPath.row])
             return cell
-            
         } else {
             return UITableViewCell(style: .default, reuseIdentifier: "crash incoming")
         }
     }
-    
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+
+    func tableView(_ tableView: UITableView,
+                   commit editingStyle: UITableViewCell.EditingStyle,
+                   forRowAt indexPath: IndexPath) {
         if editingStyle == .delete && viewModel?.canUserEditTeam == true {
             let index = indexPath.row
 
@@ -252,6 +288,4 @@ extension TeamUsersTabView: TeamUsersReloadDelegate {
             self.viewModel?.fetchTeamInvites()
         }
     }
-    
-    
 }
