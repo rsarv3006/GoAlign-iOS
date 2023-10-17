91aadcb9dc45dbb144c61134a81ea4b77752ccda
Robby Sarvis <rjs.app.dev@gmail.com>
2023-10-16 21:28:12 -0500

    TeamInvitesViewController - linting

--- YourTurn/TeamInvites/TeamInvitesViewController.swift
+++ YourTurn/TeamInvites/TeamInvitesViewController.swift
@@ -8,10 +8,10 @@
 import UIKit
 import Combine
 
-private let TEAM_INVITE_CELL_REUSE_ID = "teamInviteCell"
-private let TEAM_INVITE_SUB_CELL_REUSE_ID = "teamInviteSubCell"
+private let TEAMINVITECELLREUSEID = "teamInviteCell"
+private let TEAMINVITESUBCELLREUSEID = "teamInviteSubCell"
 
-protocol TeamInvitesViewControllerDelegate {
+protocol TeamInvitesViewControllerDelegate: AnyObject {
     func modifyLoaderState(shouldShowLoader: Bool)
 }
 
@@ -36,8 +36,8 @@ class TeamInvitesViewController: UITableViewController {
         view.backgroundColor = .customBackgroundColor
         title = "Team Invites"
 
-        tableView.register(TeamInviteCellView.self, forCellReuseIdentifier: TEAM_INVITE_CELL_REUSE_ID)
-        tableView.register(TeamInviteSubCellView.self, forCellReuseIdentifier: TEAM_INVITE_SUB_CELL_REUSE_ID)
+        tableView.register(TeamInviteCellView.self, forCellReuseIdentifier: TEAMINVITECELLREUSEID)
+        tableView.register(TeamInviteSubCellView.self, forCellReuseIdentifier: TEAMINVITESUBCELLREUSEID)
         tableView.rowHeight = 60
 
         viewModel?.fetchCurrentInvites()
@@ -51,7 +51,9 @@ class TeamInvitesViewController: UITableViewController {
             self.showLoader(false)
             switch result {
             case .failure(let error):
-                self.showMessage(withTitle: "Uh Oh", message: "Unexpected error fetching invites. \(error.localizedDescription)")
+                self.showMessage(
+                    withTitle: "Uh Oh",
+                    message: "Unexpected error fetching invites. \(error.localizedDescription)")
             case .success(let teamInvites):
                 self.teamInvites = teamInvites
             }
@@ -74,12 +76,20 @@ extension TeamInvitesViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.row == 0 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: TEAM_INVITE_CELL_REUSE_ID, for: indexPath) as! TeamInviteCellView
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: TEAMINVITECELLREUSEID,
+                // swiftlint:disable:next force_cast
+                for: indexPath) as! TeamInviteCellView
             let invite = teamInvites[indexPath.section].inviteModel
-            cell.viewModel = TeamInviteCellVM(teamName: invite.team.teamName, inviterName: invite.inviteCreator.username)
+            cell.viewModel = TeamInviteCellVM(
+                teamName: invite.team.teamName,
+                inviterName: invite.inviteCreator.username)
             return cell
         } else {
-            let cell = tableView.dequeueReusableCell(withIdentifier: TEAM_INVITE_SUB_CELL_REUSE_ID, for: indexPath) as! TeamInviteSubCellView
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: TEAMINVITESUBCELLREUSEID,
+                // swiftlint:disable:next force_cast
+                for: indexPath) as! TeamInviteSubCellView
             let inviteId = teamInvites[indexPath.section].inviteModel.teamInviteId
 
             cell.viewModel = TeamInviteSubCellVM(inviteId: inviteId)
