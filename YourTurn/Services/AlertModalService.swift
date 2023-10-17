c295c899697e395f3a5efca35ad0f7148d55bfb7
Robby Sarvis <rjs.app.dev@gmail.com>
2023-10-16 22:14:46 -0500

    AlertModalService - linting

--- YourTurn/Services/AlertModalService.swift
+++ YourTurn/Services/AlertModalService.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 struct AlertModalService {
-    static func openAlert(viewController: UIViewController, modalMessage: String, modalTitle: String = "", modalCloseText: String = "Close") {
+    static func openAlert(
+        viewController: UIViewController,
+        modalMessage: String,
+        modalTitle: String = "",
+        modalCloseText: String = "Close") {
         DispatchQueue.main.async {
             let alert = UIAlertController(title: modalTitle, message: modalMessage, preferredStyle: .alert)
             let alertAction = UIAlertAction(title: modalCloseText, style: .default) { _ in
